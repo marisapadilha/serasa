22627fb51239dd89a3f45f04301e0b54f44f830f
marisapadilha <marisa18padilha@gmail.com>
2020-08-21 20:08:37 -0300

    retirado import

--- src/global.js
+++ src/global.js
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700');
     
     * {
         margin: 0;
